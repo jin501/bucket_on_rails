27c05c5dfb2e462c2f3c5394ebc952dcc6877c9b
jin501 <soljin01@gmail.com>
2017-02-25 16:28:30 -0500

    Add some styling

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,4 +1,7 @@
 var documentready = function(){
+  console.log("%c    \u2600SOL          ", "background: pink; color: yellow; font-size: x-large");
+  console.log("%c  solthewebdev@gmail.com   ", "background: pink; color: yellow; font-size: medium");
+  console.log("%c          \u00a9 2017        ", "background: pink; color: yellow; font-size: large");
 
   $('#follow').on('click', (e)=> {
     e.preventDefault();
